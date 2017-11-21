3de06e3d5bc312285a8e358a9c96e75ae2c78a3e
Sotis <alexander.sotis@gtech.com>
2017-11-21 17:09:12 -0500

    Add existing file

---
+++ src/stockticker/StockPricer.java
@@ -0,0 +1,68 @@
+package stockticker;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class StockPricer {
+
+
+	
+	public StockPricer(){
+
+	}// End constructor
+	
+	
+	public String getPrice(String ticker) throws IOException{
+		URL url = new URL("https://finance.google.com/finance?q=NASDAQ:"+ticker);
+		URLConnection urlConn = url.openConnection();
+		InputStreamReader inStream = new InputStreamReader(urlConn.getInputStream());
+		BufferedReader buff = new BufferedReader(inStream);
+		String price = "not found";
+		String line = buff.readLine();
+
+		boolean keepGoing = true;
+		while(line != null  && keepGoing){
+
+			if(line.contains("[\""+ticker+"\",")){
+
+				int target = line.indexOf("[\""+ticker+"\",");
+				
+				int deci = line.indexOf("\"", target);
+				deci = line.indexOf("\"", deci+1);
+				deci = line.indexOf("\"", deci+1);
+				deci = line.indexOf("\"", deci+1);
+				deci = line.indexOf(".", deci+1);
+
+				
+				int start = deci;
+				while(line.charAt(start) != '\"'){
+					start--;
+				}
+				price = line.substring(start + 1, deci +3);
+				keepGoing = false;
+				
+			}
+			
+			line = buff.readLine();
+			
+		}
+
+		price = price.replace(",","");
+		
+		return  price;
+		
+	}// End getPrice
+	
+	
+	private static void print(Object message){		
+		System.out.println(message);
+	}// End print
+	
+
+	
+}// End Stock
